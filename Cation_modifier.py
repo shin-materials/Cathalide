711f8ec05a787513e0287a863e0b2be79b491f23
Yongjin Shin <yongjin.shin@u.northwestern.edu>
2021-04-07 05:04:34 -0700

    Implemented poscar writing part

--- Cation_modifier.py
+++ Cation_modifier.py
@@ -7,8 +7,8 @@ from Functions import molecule_rotation, convert_site_index
 import copy
 import pandas as pd
 
-# struct = Structure.from_file("Test_structures/FPB_bulk_cubic.vasp")
-struct = Structure.from_file("test.vasp")
+struct = Structure.from_file("Test_structures/FPB_bulk_cubic.vasp")
+#struct = Structure.from_file("test.vasp")
 
 # Read bond data
 # Input data from user: what elements composing the molecule
@@ -185,6 +185,90 @@ for atom in molecule:
     translated_struct.sites[df[df['atom_label']==atom]['site_index'].iloc[0]].frac_coords=translated_coords
     
 translated_struct.to(fmt='poscar',filename='test2.vasp')
+
+
+
+######### FUNCTIONS ###########
+def Write_POSCAR(filename,pmg_struct,element_sequence=None):
+    """
+    Parameters
+    ----------
+    filename : str
+        name of POSCAR file. PatternLength_PatternNumber_TransVector.vasp
+    CN_str : str
+        Sequence of numbers indicating coordination number of B-cations
+    lattice : numpy array
+        3x3 array describing lattice vectors
+    A_array : numpy array
+        n x 3 array; atomic position of A-caitons.
+    B_array : numpy array
+        n x 3 array; atomic position of B-caitons.
+    O_array : numpy array
+        list of lists. Sub-lists have 3 numbers representing position of oxygen atoms
+    A : str
+        Name of A-cation if not defined, Sr is used.
+    B : str
+        Name of B-cation if not defined, Fe is used.
+    Returns
+    -------
+    None.
+    """
+    
+    out_file=open(filename,'w')
+    out_file.write("Generated POSCAR file\n") #first comment line
+    out_file.write("1.0\n") # scale 
+    for i in range(np.shape(lattice)[0]):
+        out_file.write("{0:20.10f} {1:20.10f} {2:20.10f}\n".format(lattice[i,0],lattice[i,1],lattice[i,2]))
+    out_file.write("   {0}   {1}   O\n".format(A,B)) # Atom labels
+    out_file.write("   {0}    {1}   {2}\n".format(np.shape(A_array)[0],
+                                              np.shape(B_array)[0],
+                                              np.shape(O_array)[0]))
+    out_file.write("   Direct\n")
+    for i in range(np.shape(A_array)[0]):
+        out_file.write("{0:16.10f} {1:19.10f} {2:19.10f}\n".format(A_array[i,0],A_array[i,1],A_array[i,2]))
+    # for i,B_site in enumerate(B_list):
+    #     out_file.write("    "+"        ".join('%.10f' % entry for entry in B_site))
+    #     out_file.write("\n")
+    #     out_file.close()
+    for i in range(np.shape(B_array)[0]):
+        out_file.write("{0:16.10f} {1:19.10f} {2:19.10f}\n".format(B_array[i,0],B_array[i,1],B_array[i,2]))
+    for i in range(np.shape(O_array)[0]):
+        out_file.write("{0:16.10f} {1:19.10f} {2:19.10f}\n".format(O_array[i,0],O_array[i,1],O_array[i,2]))
+    # Printing oxygen sites is a bit different as it is with list format
+    # for i,O_site in enumerate(O_list):
+    #     out_file.write("    "+"        ".join('%.10f' % entry for entry in O_site))
+    #     out_file.write("\n")
+    #     out_file.close()
+
+
+
+
+    ## Definition of lattice vectors
+# lattice = np.array([[a*np.sqrt(2), 0, 0],
+#                     [0, a*np.sqrt(6), 0],
+#                     [a*np.sqrt(2)*((t*3)%1),a*np.sqrt(6)*t,a/np.sqrt(3)]])
+lattice = struct.lattice.matrix
+out_file=open('test.vasp','w')
+out_file.write("Generated POSCAR file\n") #first comment line
+out_file.write("1.0\n") # scale 
+for i in range(np.shape(lattice)[0]):
+    out_file.write("{0:20.10f} {1:20.10f} {2:20.10f}\n".format(lattice[i,0],lattice[i,1],lattice[i,2]))
+species_list=struct.species
+reduced_species=[str(i) for n, i in enumerate(species_list) if i not in species_list[:n]]
+out_file.write("  "+"  ".join('%3s' % entry for entry in reduced_species))
+out_file.write("\n")
+num_each_element=[species_list.count(Element(i)) for i in reduced_species]
+out_file.write("  "+"  ".join('%3d' % entry for entry in num_each_element))
+out_file.write("\n")
+out_file.write("Direct\n")
+for element in reduced_species:
+    site_list=df[df['element']=='H']['pmg_site']
+    for site in site_list:
+        out_file.write("  "+"        ".join('%.10f' % entry for entry in site.frac_coords)+'\n')
+out_file.close()
+## Write_POSCAR
+#Write_POSCAR('write_test.vasp',struct)
     
     
+        
     
