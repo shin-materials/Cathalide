Corrected error in rotation function

Cation_modifier.py
@@ -7,7 +7,7 @@ from Functions import molecule_rotation, convert_site_index, Write_POSCAR
 import copy
 import pandas as pd
 
-struct = Structure.from_file("Test_structures/FPB_bulk_cubic.vasp")
+struct = Structure.from_file("FPB_FA_arranged_multiple_rotation.vasp")
 #struct = Structure.from_file("test.vasp")
 
 # Read bond data
@@ -39,8 +39,30 @@ for i in range(0,struct.num_sites):
                 'element':str((struct.sites[i]).specie)},ignore_index=True)
 ################################################################
 
+def update_dataframe(struct):
+    ############## USING PANDAS DATAFRAME ##########################
+    n_atom_count_dict=dict()
+    df=pd.DataFrame(columns=['site_index','atom_label','pmg_site','element'])
+    for i in range(0,struct.num_sites):
+        # Update label for each element
+        if struct[i].specie in list(n_atom_count_dict.keys()):
+            n_atom_count_dict.update({struct[i].specie:n_atom_count_dict[struct[i].specie]+1})
+        else:
+            n_atom_count_dict.update({struct[i].specie:1})
+            
+        label='{0}{1}'.format(struct.species[i], n_atom_count_dict[struct[i].specie])
+        # Append a site to the data frame
+        # If this part is costly, maybe using pd.concat would be faster (not sure yet)
+        df= df.append({'site_index':i, \
+                    'atom_label':'{0}{1}'.format(struct.species[i], n_atom_count_dict[struct[i].specie]), \
+                    'pmg_site':struct.sites[i],\
+                    'element':str((struct.sites[i]).specie)},ignore_index=True)
+    return (df, n_atom_count_dict)
+    ################################################################
+
+
 molecules_list=[]
-molecule=['C1','N1','N2','H1','H2','H3','H4','H5']
+molecule=['C3','N5','N6','H11','H12','H13','H14','H15']
 molecules_list.append(molecule)
 
 
@@ -150,15 +172,26 @@ while loop_flag == 1:
     1. Center of mass from mass-weighted average
     2. Volumetric center of mass..? by using convex hull?
 '''
-    
+"""
+ROTATION: Error with periodic boundary condition
+""" 
 
 
 # axis_vector and rotation angle
+molecule=['C1', 'N2', 'H1', 'N1', 'H4', 'H5', 'H3', 'H2']
 reference_point=(df[df['atom_label']=='C1']['pmg_site'].iloc[0]).frac_coords
-axis_vector=np.array([0,1,0])  ## To be modified
-angle_degree = 45 # degree
-struct=molecule_rotation(struct,molecule,df,
-                          axis_vector,angle_degree,reference_point)
+axis_vector=np.array([1,1,0])  ## To be modified
+angle_degree = 90 # degree
+struct=molecule_rotation(struct,molecule,df,axis_vector,angle_degree,reference_point)
+
+reference_point=(df[df['atom_label']=='C3']['pmg_site'].iloc[0]).frac_coords
+molecule=['C3','N5','N6','H11','H12','H13','H14','H15']
+axis_vector=np.array([-1,1,0])  ## To be modified
+angle_degree = 90 # degree
+struct=molecule_rotation(struct,molecule,df,axis_vector,angle_degree,reference_point)
+
+
+
 struct.to(fmt='poscar',filename='test2.vasp')
 
 
@@ -171,14 +204,26 @@ for atom in molecule:
     translated_coords = coords + translation_vector
     translated_struct.sites[df[df['atom_label']==atom]['site_index'].iloc[0]].frac_coords=translated_coords
     
-translated_struct.to(fmt='poscar',filename='test2.vasp')
-
-
+#translated_struct.to(fmt='poscar',filename='test2.vasp')
 
 
 ## Write_POSCAR
-Write_POSCAR('write_test.vasp',struct)
-    
+# Write_POSCAR('write_test.vasp',struct,label_df=df)
+
+# copy molecule
+"""
+Fucntionality:
+    0. Remove a cation
+    struct.remove_sites([0])
+    struct.remove_sites([df[df['atom_label']==atom]['site_index'].iloc[0]])
+    1. copy to specific site (copy and translate)
+        
+        add to n_atom_cound_dict
+        struct.sites.append(A1)
+    2. 
+
+"""
+
     
         
     

Functions.py
@@ -28,6 +28,158 @@ def convert_site_index(label2site_index,str_atom):
     return pmg_site_index
 
 
+
+
+
+def molecule_rotation(pmg_struct,molecule,label_df,axis_vector,angle,reference_point):
+	"""
+	input:
+	pmg_struct: pymatgen structure. Structure containing organic molecule to rotate
+	molecule: list of strings, containing atomic labels composing molecule to rotate
+				ex) molecule=['C1','N1','N2','H1','H2','H3','H4','H5']
+	label_df: Pandas DataFrame with columns=['site_index','atom_label','pmg_site','element']
+	axis_vector: numpy array with size (3,). This defines the direction of rotation axis
+				This function will normalize the vector.
+	angle - float: degree angles to rotate the molecule with respect to the rotation axis.
+				The rotation will be done anti-clockwise
+	reference_point - numpy array with size (3,). This is a fractional coordinate of point 
+				that the rotation axis goes through
+	
+	output:
+	rotated_struct: pymatgen structure 
+	"""
+	rotated_struct=copy.deepcopy(pmg_struct)
+	
+	# convert reference point to cartesian
+	a=pmg_struct.lattice.a
+	b=pmg_struct.lattice.b
+	c=pmg_struct.lattice.c
+	reference_point = reference_point * np.array([a,b,c])
+
+	## Transformation (2)
+	## ROTATION VECTOR
+	## Vector from np.array (should be normalized),
+	## and multiply the angle of rotation (in radian)
+	## Ex) r = R.from_rotvec(np.pi/2 * np.array([0, 0, 1]))
+	# convert to real space axis_vector by introducing lattice vector
+	
+	axis_vector = axis_vector * np.array([a,b,c])
+	#axis_vector=axis_vector * np.array([a,b,c])
+	# normalize
+	axis_vector = axis_vector/np.linalg.norm(axis_vector)
+	# make degree to radian
+	angle_radian = angle / 180 * np.pi
+	# create rotation operator
+	rotation=R.from_rotvec(angle_radian * axis_vector)
+
+	for atom in molecule:
+		# Get index from label of atom,
+		# call sites from pymatgen struct
+		# and parse the fractional coordinates
+		#coords=copy.deepcopy((label_df[label_df['atom_label']==atom]['pmg_site'].iloc[0]).frac_coords)
+		coords=copy.deepcopy((label_df[label_df['atom_label']==atom]['pmg_site'].iloc[0]).coords)
+		
+		## Transformation (1)
+		## subtract the coordinate of rotation-axis-position  = reference point
+		coords_diff = coords-reference_point
+		# adjust for when the operation includes the periodic boundary
+		adjust_array= -np.array([a,b,c])*(coords_diff > np.array([a*0.5,b*0.5,c*0.5])).astype(int) + \
+					np.array([a,b,c])*(coords_diff < np.array([-a*0.5,-b*0.5,-c*0.5])).astype(int)
+		coords_temp = coords_diff + adjust_array
+		shift_vector = coords_temp - coords
+
+		# apply rotation
+		coords_temp=rotation.apply(coords_temp)
+		# Move to reference point again
+		coords = coords_temp - shift_vector
+		rotated_struct.sites[label_df[label_df['atom_label']==atom]['site_index'].iloc[0]].coords=coords
+
+	return rotated_struct
+
+
+
+def molecule_translation(pmg_struct,molecule,label_df,translation_vector):
+	"""
+	input:
+	pmg_struct: pymatgen structure. 
+		Structure containing organic molecule to rotate
+	molecule: list of strings
+		 containing atomic labels composing molecule to rotate
+				ex) molecule=['C1','N1','N2','H1','H2','H3','H4','H5']
+	label2site_index: dictionary defined in script
+	translation_vector: List of size 3, or numpy array with size (3,)
+		This defines the vector of translation of the molecule
+	angle - float: degree angles to rotate the molecule with respect to the rotation axis.
+				The rotation will be done anti-clockwise
+	reference_point - numpy array with size (3,). This is a fractional coordinate of point 
+				that the rotation axis goes through
+	
+	output:
+	translated_struct: pymatgen structure 
+
+	"""
+	# convert to numpy array
+	translation_vector=np.array(translation_vector)
+
+	translated_struct=copy.deepcopy(pmg_struct)
+	for atom in molecule:
+	    coords = copy.deepcopy((label_df[label_df['atom_label']==atom]['pmg_site'].iloc[0]).frac_coords)
+	    translated_coords = coords + translation_vector
+	    translated_struct.sites[label_df[label_df['atom_label']==atom]['site_index'].iloc[0]].frac_coords=translated_coords
+	    
+	rotated_struct=copy.deepcopy(pmg_struct)
+
+	return translated_struct
+
+######### FUNCTIONS ###########
+def Write_POSCAR(filename,struct, label_df, element_sequence=None):
+    """
+    Parameters
+    ----------
+    filename : str
+        name of POSCAR file. PatternLength_PatternNumber_TransVector.vasp
+    struct : pymatgen Structure object
+        structure object to write as POSCAR
+    DF : Pandas dataframe
+        Pandas DataFrame with columns=['site_index','atom_label','pmg_site','element']
+    element_sequence : str
+        Sequence of elements to be written in POSCAR
+        ex) 'CNHPbI' or 'C N H Pb I' will affect the 7th line in the POSCAR file
+    Returns
+    -------
+    None.
+    """
+    # Idenfity lattice vectors
+    lattice = struct.lattice.matrix
+    # Get element of each atoms
+    species_list=struct.species
+    # Remove repeated entry in the element list
+    reduced_species=[str(i) for n, i in enumerate(species_list) if i not in species_list[:n]]
+    
+    out_file=open(filename,'w')
+    out_file.write("Generated POSCAR file\n") #first comment line
+    out_file.write("1.0\n") # scale 
+    # Print lattice part
+    for i in range(np.shape(lattice)[0]):
+        out_file.write("{0:20.10f} {1:20.10f} {2:20.10f}\n".format(lattice[i,0],lattice[i,1],lattice[i,2]))
+    # Print elements
+    out_file.write("  "+"  ".join('%3s' % entry for entry in reduced_species))
+    out_file.write("\n")
+    # Print the number of atoms for each element
+    num_each_element=[species_list.count(Element(i)) for i in reduced_species]
+    out_file.write("  "+"  ".join('%3d' % entry for entry in num_each_element))
+    out_file.write("\n")
+    
+    out_file.write("Direct\n")
+    for element in reduced_species:
+        site_list=label_df[label_df['element']=='H']['pmg_site']
+        for site in site_list:
+            out_file.write("  "+"        ".join('%.10f' % entry for entry in site.frac_coords)+'\n')
+    out_file.close()
+    
+    return
+
+
 def molecule_rotation_bak(pmg_struct,molecule,label2site_index,axis_vector,angle,reference_point):
 	"""
 	input:
@@ -75,149 +227,3 @@ def molecule_rotation_bak(pmg_struct,molecule,label2site_index,axis_vector,angle
 	    rotated_struct.sites[label2site_index[atom]].frac_coords=coords
 
 	return rotated_struct
-
-def molecule_rotation(pmg_struct,molecule,label_df,axis_vector,angle,reference_point):
-	"""
-	input:
-	pmg_struct: pymatgen structure. Structure containing organic molecule to rotate
-	molecule: list of strings, containing atomic labels composing molecule to rotate
-				ex) molecule=['C1','N1','N2','H1','H2','H3','H4','H5']
-	label_df: Pandas DataFrame with columns=['site_index','atom_label','pmg_site','element']
-	axis_vector: numpy array with size (3,). This defines the direction of rotation axis
-				This function will normalize the vector.
-	angle - float: degree angles to rotate the molecule with respect to the rotation axis.
-				The rotation will be done anti-clockwise
-	reference_point - numpy array with size (3,). This is a fractional coordinate of point 
-				that the rotation axis goes through
-	
-	output:
-	rotated_struct: pymatgen structure 
-	"""
-	rotated_struct=copy.deepcopy(pmg_struct)
-
-	for atom in molecule:
-	    # Get index from label of atom,
-	    # call sites from pymatgen struct
-	    # and parse the fractional coordinates
-	    coords=copy.deepcopy((label_df[label_df['atom_label']==atom]['pmg_site'].iloc[0]).frac_coords)
-	    
-	    ## Transformation (1)
-	    ## subtract the coordinate of rotation-axis-position  = reference point
-	    coords_temp=coords-reference_point
-	    
-	    ## Transformation (2)
-	    ## ROTATION VECTOR
-	    ## Vector from np.array (should be normalized),
-	    ## and multiply the angle of rotation (in radian)
-	    ## Ex) r = R.from_rotvec(np.pi/2 * np.array([0, 0, 1]))
-
-	    # normalize
-	    axis_vector=axis_vector/np.linalg.norm(axis_vector)
-	    # make degree to radian
-	    angle_radian = angle / 180 * np.pi
-	    rotation=R.from_rotvec(angle_radian * axis_vector)
-	    # apply rotation
-	    coords_temp=rotation.apply(coords_temp)
-	    # Move to reference point again
-	    coords=coords_temp+reference_point
-	    rotated_struct.sites[label_df[label_df['atom_label']==atom]['site_index'].iloc[0]].frac_coords=coords
-
-	return rotated_struct
-
-
-
-def molecule_translation(pmg_struct,molecule,label2site_index,axis_vector,angle,reference_point):
-	"""
-	input:
-	pmg_struct: pymatgen structure. Structure containing organic molecule to rotate
-	molecule: list of strings, containing atomic labels composing molecule to rotate
-				ex) molecule=['C1','N1','N2','H1','H2','H3','H4','H5']
-	label2site_index -- dictionary defined in script
-	axis_vector: numpy array with size (3,). This defines the direction of rotation axis
-				This function will normalize the vector.
-	angle - float: degree angles to rotate the molecule with respect to the rotation axis.
-				The rotation will be done anti-clockwise
-	reference_point - numpy array with size (3,). This is a fractional coordinate of point 
-				that the rotation axis goes through
-	
-	output:
-	rotated_struct: pymatgen structure 
-	"""
-	rotated_struct=copy.deepcopy(pmg_struct)
-
-	for atom in molecule:
-	    # Get index from label of atom,
-	    # call sites from pymatgen struct
-	    # and parse the fractional coordinates
-	    coords=copy.deepcopy(pmg_struct.sites[label2site_index[atom]].frac_coords)
-	    
-	    ## Transformation (1)
-	    ## subtract the coordinate of rotation-axis-position  = reference point
-	    coords_temp=coords-reference_point
-	    
-	    ## Transformation (2)
-	    ## ROTATION VECTOR
-	    ## Vector from np.array (should be normalized),
-	    ## and multiply the angle of rotation (in radian)
-	    ## Ex) r = R.from_rotvec(np.pi/2 * np.array([0, 0, 1]))
-
-	    # normalize
-	    axis_vector=axis_vector/np.linalg.norm(axis_vector)
-	    # make degree to radian
-	    angle_radian = angle / 180 * np.pi
-	    rotation=R.from_rotvec(angle_radian * axis_vector)
-	    # apply rotation
-	    coords_temp=rotation.apply(coords_temp)
-	    # Move to reference point again
-	    coords=coords_temp+reference_point
-	    rotated_struct.sites[label2site_index[atom]].frac_coords=coords
-
-	return rotated_struct
-
-######### FUNCTIONS ###########
-def Write_POSCAR(filename,struct, DF=df, element_sequence=None):
-    """
-    Parameters
-    ----------
-    filename : str
-        name of POSCAR file. PatternLength_PatternNumber_TransVector.vasp
-    struct : pymatgen Structure object
-        structure object to write as POSCAR
-    DF : Pandas dataframe
-        Pandas DataFrame with columns=['site_index','atom_label','pmg_site','element']
-    element_sequence : str
-        Sequence of elements to be written in POSCAR
-        ex) 'CNHPbI' or 'C N H Pb I' will affect the 7th line in the POSCAR file
-    Returns
-    -------
-    None.
-    """
-    # Idenfity lattice vectors
-    lattice = struct.lattice.matrix
-    # Get element of each atoms
-    species_list=struct.species
-    # Remove repeated entry in the element list
-    reduced_species=[str(i) for n, i in enumerate(species_list) if i not in species_list[:n]]
-    
-    out_file=open(filename,'w')
-    out_file.write("Generated POSCAR file\n") #first comment line
-    out_file.write("1.0\n") # scale 
-    # Print lattice part
-    for i in range(np.shape(lattice)[0]):
-        out_file.write("{0:20.10f} {1:20.10f} {2:20.10f}\n".format(lattice[i,0],lattice[i,1],lattice[i,2]))
-    # Print elements
-    out_file.write("  "+"  ".join('%3s' % entry for entry in reduced_species))
-    out_file.write("\n")
-    # Print the number of atoms for each element
-    num_each_element=[species_list.count(Element(i)) for i in reduced_species]
-    out_file.write("  "+"  ".join('%3d' % entry for entry in num_each_element))
-    out_file.write("\n")
-    
-    out_file.write("Direct\n")
-    for element in reduced_species:
-        site_list=DF[DF['element']=='H']['pmg_site']
-        for site in site_list:
-            out_file.write("  "+"        ".join('%.10f' % entry for entry in site.frac_coords)+'\n')
-    out_file.close()
-    
-    return